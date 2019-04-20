b387b7f921ab7b962b3ea2604fffa2937850a9df
Ronyonka <rononyonka@gmail.com>
2019-04-20 16:21:59 +0300

    added code to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.qu
+
+
+    //getting the sound going
+    pads.forEach((pad, index) =>{
+        pad.addEventListener('click',function(){
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
+});
